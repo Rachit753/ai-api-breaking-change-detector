2959a8944d15a503d08c833a4e0b8535e6c394be
Rachit753 <chauhanrachit02@gmail.com>
2026-02-06 10:35:53 +0530

    Database connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+require("./config/db"); // connect DB
+
 const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
@@ -6,3 +8,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
